23fc70d716181b145d1f67db2820db7759a383f9
秦聪 <1309740551@qq.com>
2015-09-07 17:03:23 +0800

    jiasd

---
+++ JavaThinking/a.java
@@ -0,0 +1,20 @@
+enum Search {HITHER,YON,}
+public class HelloJava {
+	public static void main(String[] args){
+		Search[] vals = Search.values();
+		Enum e = Search.HITHER;
+		for(Enum en : e.getClass().getEnumConstants())
+			System.out.println(en);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
