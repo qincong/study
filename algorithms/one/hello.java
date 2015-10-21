35d6f379b5d248afda309ec9c1cbf78e19685572
秦聪 <1309740551@qq.com>
2015-10-21 23:13:42 +0800

    no summary

--- algorithms/one/hello.java
+++ algorithms/one/hello.java
@@ -1,8 +1,9 @@
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdDraw;
 public class hello {
 	public static void main(String args[]) {
-		System.out.println(args[0]);
-		StdOut.println(args[1]);
-		StdOut.println("hi");
+		double a=StdIn.readDouble();
+		StdOut.println(a);
 	}
 }
