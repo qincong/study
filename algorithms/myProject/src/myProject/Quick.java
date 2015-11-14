9a308634cf670e18187af6d40785b71f4839da0c
秦聪 <1309740551@qq.com>
2015-11-14 17:07:27 +0800

    快速排序的三项切分

--- algorithms/myProject/src/myProject/Quick.java
+++ algorithms/myProject/src/myProject/Quick.java
@@ -11,27 +11,18 @@ public class Quick {
 	private static void sort(int[] a,int lo,int hi) {
 		if(hi<=lo)
 			return;
-		int j=partition(a,lo,hi);
-		sort(a,lo,j-1);
-		sort(a,j+1,hi);
-	}
-	private static int partition(int[] a,int lo,int hi) {
-		int i=lo,j=hi+1;
+		int lt=lo,i=lo+1,gt=hi;
 		int v=a[lo];
-		while(true) {
-			while(a[++i]<v) {
-				if(i==hi)
-					break;
-			}
-			while(a[--j]>v) {
-				if(j==lo)
-					break;
-			}
-			if(i>=j)
-				break;
+		while(i<=gt) {
+			if(a[i]<v)
+				tools.exch(a, i++, lt++);
+			if(a[i]>v)
+				tools.exch(a, i, gt--);
+			if(a[i]==v)
+				i++;
 		}
-		tools.exch(a,lo,j);
-		return j;
+		sort(a,lo,lt-1);
+		sort(a,gt+1,hi);
 	}
 	public static void main(String[] args) {
 		int []a={3,5,1,56,34,8,2};
