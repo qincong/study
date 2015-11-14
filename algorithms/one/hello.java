5812ff8fd7a4f13250ebc4291bf36450b07ee315
秦聪 <1309740551@qq.com>
2015-11-14 12:37:59 +0800

    这是Android

--- algorithms/one/hello.java
+++
@@ -1,9 +0,0 @@
-import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdDraw;
-public class hello {
-	public static void main(String args[]) {
-		double a=StdIn.readDouble();
-		StdOut.println(a);
-	}
-}
